fe4416a92f4d7ed46204ac2a5bd35ca7586c8099
Joshua Sinyor <>
2021-03-10 16:50:30 +0000

    Update test_helpers.rb. Reflects error correction.

--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -17,7 +17,7 @@ TEST_DATE_4 = "21-07-2010"
 TEST_DATE_5 = "13-11-2011"
 TEST_DATE_6 = "00-01-2001" # Impossible day
 TEST_DATE_7 = "29-02-2001" # Impossible day
-TEST_DATE_8 = "01-01-2099" # Impossible day
 TEST_DATE_9 = "04-13-2001" # Impossible month
+TEST_DATE_8 = "01-01-2099" # Impossible year
 
 RANDOM_DATE = Time.at(rand * Time.now.to_i).strftime("%d-%m-%Y")
