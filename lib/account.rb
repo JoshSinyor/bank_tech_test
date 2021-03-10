7911adc738518a6d1a54d834d88146654155e945
Joshua Sinyor <>
2021-03-10 17:36:07 +0000

    Create bank_tech_test.rb. Update account.rb. Reflects creation of Ruby script and updating of relative requirements.

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'transaction'
+
 # Instances of this class are accounts.
 class Account
   attr_reader :transactions_array
---
+++ lib/bank_tech_test.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'account'
+require 'irb'
+
+binding.irb
