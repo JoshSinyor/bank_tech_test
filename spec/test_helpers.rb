c57c6bc1948dcd4c55145b5e36b45c738840e23c
Joshua Sinyor <>
2021-03-11 17:44:26 +0000

    Update test_helpers.rb. Reflects switch to forward-slash delineation.

--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -12,17 +12,17 @@ TEST_SUM_7 = 256
 TEST_SUM_8 = 512
 TEST_SUM_9 = -1 # Impossible number
 
-RANDOM_DATE = Time.at(rand * Time.now.to_i).strftime('%d-%m-%Y')
+RANDOM_DATE = Time.at(rand * Time.now.to_i).strftime('%d/%m/%Y')
 
-TEST_DATE_1 = '01-01-2001'
-TEST_DATE_2 = '02-01-2001'
-TEST_DATE_3 = '01-02-2002'
-TEST_DATE_4 = '21-07-2010'
-TEST_DATE_5 = '13-11-2011'
-TEST_DATE_6 = '00-01-2001' # Impossible day
-TEST_DATE_7 = '29-02-2001' # Impossible day
-TEST_DATE_9 = '04-13-2001' # Impossible month
-TEST_DATE_8 = '01-01-2099' # Impossible year
+TEST_DATE_1 = '01/01/2001'
+TEST_DATE_2 = '02/01/2001'
+TEST_DATE_3 = '01/02/2002'
+TEST_DATE_4 = '21/07/2010'
+TEST_DATE_5 = '13/11/2011'
+TEST_DATE_6 = '00/01/2001' # Impossible day
+TEST_DATE_7 = '29/02/2001' # Impossible day
+TEST_DATE_9 = '04/13/2001' # Impossible month
+TEST_DATE_8 = '01/01/2099' # Impossible year
 
 TEST_ARRAY = [{ date: RANDOM_DATE, credit: RANDOM_SUM, debit: nil, balance: RANDOM_SUM }].freeze
 TEST_ARRAY_OUTPUT = "#{RANDOM_DATE} || #{RANDOM_SUM} || || #{RANDOM_SUM}"
