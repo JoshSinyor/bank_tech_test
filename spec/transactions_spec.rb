7a8d07a4f54bcd7839a8946ae46b419bdea35f06
Joshua Sinyor <>
2021-03-12 15:57:55 +0000

    Update transactions_spec.rb. Reflects updating of tests for Transactions class.

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,38 +1,42 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
-#
-# require 'transactions'
-#
-# describe Transactions do
-#   subject(:transactions) { described_class.new }
-#
-#   it { is_expected.to respond_to :balance }
-#   it { is_expected.to respond_to :array }
-#
-#   describe '#initialize' do
-#     it 'with a #balance of 0' do
-#       expect(transactions.balance).to eq 0
-#     end
-#
-#     it 'with an empty #transactions_array' do
-#       expect(transactions.array).to be_empty
-#     end
-#   end
-#
-#   describe '#add' do
-#     it 'to the #transactions.array' do
-#       transactions.add(RANDOM_SUM, RANDOM_DATE)
-#       expect(account.transactions.array).not_to be_empty
-#     end
-#
-#     it 'with the supplied date' do
-#       account.deposit(RANDOM_SUM, RANDOM_DATE)
-#       expect(account.transactions.array.first[:date]).to eq RANDOM_DATE
-#     end
-#
-#     it 'with the supplied sum' do
-#       account.deposit(RANDOM_SUM, RANDOM_DATE)
-#       expect(account.transactions.array.first[:credit]).to eq RANDOM_SUM
-#     end
-#   end
-# end
+
+require 'transactions'
+
+describe Transactions do
+  subject(:transactions) { described_class.new }
+
+  it { is_expected.to respond_to :balance }
+  it { is_expected.to respond_to :array }
+
+  describe '#initialize' do
+    it 'with a #balance of 0' do
+      expect(transactions.balance).to eq 0
+    end
+
+    it 'with an empty #transactions_array' do
+      expect(transactions.array).to be_empty
+    end
+  end
+
+  describe '#add' do
+    it 'to the #transactions.array' do
+      transactions.add(RANDOM_DATE, RANDOM_SUM, nil)
+      expect(transactions.array).not_to be_empty
+    end
+
+    it 'with the supplied date' do
+      transactions.add(RANDOM_DATE, nil, RANDOM_SUM)
+      expect(transactions.array.first[:date]).to eq RANDOM_DATE_OBJECT
+    end
+
+    it 'with the supplied credit sum' do
+      transactions.add(RANDOM_DATE, RANDOM_SUM, nil)
+      expect(transactions.array.first[:credit]).to eq RANDOM_SUM
+    end
+
+    it 'with the supplied debit sum' do
+      transactions.add(RANDOM_DATE, nil, RANDOM_SUM)
+      expect(transactions.array.first[:debit]).to eq RANDOM_SUM
+    end
+  end
+end
