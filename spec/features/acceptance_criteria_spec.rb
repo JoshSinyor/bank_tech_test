77d2941d7fa07b697d4b4d34acb89be6e7d64593
Joshua Sinyor <>
2021-03-13 14:23:26 +0000

    Update acceptance_criteria_spec.rb. Reflects completion of feature test.

--- spec/features/acceptance_criteria_spec.rb
+++ spec/features/acceptance_criteria_spec.rb
@@ -1,35 +1,42 @@
+# frozen_string_literal: true
+
 require 'pty'
 
-describe "Acceptance Criteria" do
-  it "Feature Test" do
+ACCEPTANCE_CRITERIA_INPUT = [
+  'account = Account.new',
+  "account.deposit(1000, '10-01-2012')",
+  "account.deposit(2000, '13-01-2012')",
+  "account.withdraw(500, '14-01-2012')",
+  'account.print_statement'
+].freeze
+
+ACCEPTANCE_CRITERIA_OUTPUT = <<~HEREDOC
+  date || credit || debit || balance
+  14/01/2012 || || 500.00 || 2500.00
+  13/01/2012 || 2000.00 || || 3000.00
+  10/01/2012 || 1000.00 || || 1000.00
+HEREDOC
+
+describe 'Feature Test' do
+  it 'meets acceptance criteria' do
     master, slave = PTY.open
     read, write = IO.pipe
-    pid = spawn("irb", :in=>read, :out=>slave)
+    spawn('irb -r ./lib/account', in: read, out: slave)
     read.close
     slave.close
 
-    write.puts "require './lib/account'"
-    write.puts "account = Account.new"
-    write.puts "account.deposit(1000, '10-01-2012')"
-    write.puts "account.deposit(2000, '13-01-2012')"
-    write.puts "account.withdraw(500, '14-01-2012')"
-    write.puts "account.print_statement"
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
-    p master.gets.chomp
+    write.puts 'irb_context.echo = false'
+    ACCEPTANCE_CRITERIA_INPUT.each { |input| write.puts input }
     write.close
+
+    output = ''
+
+    loop do
+      output += "#{master.gets.chomp}\n"
+    rescue NoMethodError
+      break
+    end
+
+    expect(output).to end_with(ACCEPTANCE_CRITERIA_OUTPUT)
   end
 end
