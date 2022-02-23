4399cab5ae7d0d5ce0ddaae83548f86bba9b3ba8
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-02-23 11:30:22 +0000

    Update printer_spec.rb. Reflects completion of testing of Printer class.

--- spec/units/printer_spec.rb
+++ spec/units/printer_spec.rb
@@ -5,36 +5,61 @@ require 'printer'
 describe Printer do
   subject(:printer) { described_class.new }
 
-  let(:test_array) { TEST_ARRAY }
-  let(:output) { capture_stdout { printer.print(test_array) } }
+  let(:stdout_output) { capture_stdout { printer.print(TEST_ARRAY) } }
+  let(:array_output) { stdout_output.split("\n") }
+  let(:subarray_output) do
+    array_output.map! { |x| x.split('||') }
+    array_output.map! { |x| x.map!(&:strip) }
+  end
+  let(:random_subarray_output) { subarray_output[1..].sample }
 
   it { is_expected.to respond_to :print }
 
   describe '#print_header' do
     it 'on the first line' do
-      expect(output).to start_with(Printer::HEADER)
+      expect(stdout_output).to start_with(Printer::HEADER)
     end
 
     it 'even if the #array is empty' do
-      output = capture_stdout { printer.print([]) }
-      expect(output).to start_with(Printer::HEADER)
+      stdout_output = capture_stdout { printer.print([]) }
+      expect(stdout_output).to start_with(Printer::HEADER)
     end
   end
 
   describe '#process_array' do
     it 'in descending date order' do
-      array = []
-      output.split("\n").each { |x| array.push x[0, 10] }
+      subarray_output[1..-2].each_with_index do |x, i|
+        expect(DateTime.parse(x[0]).jd).to be >= DateTime.parse(subarray_output[i + 2][0]).jd
+      end
+    end
+  end
 
-      array[1..-2].each_with_index do |x, i|
-        expect(DateTime.parse(x).jd).to be >= DateTime.parse(array[i + 2]).jd
+  describe '#add_balance' do
+    it 'to each transaction' do
+      subarray_output[1..].each do |x|
+        expect(x[3]).to match(/[0-9]*.[0-9]{2}/)
       end
     end
   end
 
   describe '#print_array' do
+    it 'to stdout' do
+      expect(stdout_output).to eq TEST_ARRAY_OUTPUT
+    end
+
     it 'starting on the second line' do
-      expect(output.split("\n")[1]).to eq TEST_ARRAY_OUTPUT.split("\n")[1]
+      expect(array_output[1]).to match(%r{[0-9]{2}/[0-9]{2}/[0-9]{4}( \|\|){1,2}( [0-9]*\.[0-9]{2})( \|\|){1,2}( [0-9]*\.[0-9]{2})})
+    end
+  end
+
+  describe '#format_row' do
+    it 'dates as DD/MM/YYYY' do
+      expect(random_subarray_output[0]).to match(%r{[0-9]{2}/[0-9]{2}/[0-9]{4}})
+    end
+
+    it 'sums as #.##' do
+      expect(/[0-9]*\.[0-9]{2}/).to match(random_subarray_output[1]).or match(random_subarray_output[2])
+      expect(/[0-9]*\.[0-9]{2}/).to match(random_subarray_output[3])
     end
   end
 end
